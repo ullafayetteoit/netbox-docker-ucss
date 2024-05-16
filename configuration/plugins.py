30375ce9a1d2b4fd33b792a9214c11a6f7c9c961
UL Lafayette Office of Information Technology <ullafayetteoit-github@louisiana.edu>
2024-05-16 10:33:05 -0500

    Initial Netbox 4 build

--- configuration/plugins.py
+++ configuration/plugins.py
@@ -11,3 +11,16 @@
 #     ADD YOUR SETTINGS HERE
 #   }
 # }
+
+# # Enable installed plugins. Add the name of each plugin to the list.
+PLUGINS = ['netbox_dns']
+
+# # Plugins configuration settings. These settings are used by various plugins that the user may have installed.
+# # Each key in the dictionary is the name of an installed plugin and its value is a dictionary of settings.
+PLUGINS_CONFIG = {
+  'netbox_dns': {
+    'tolerate_non_rfc1035_types': ["TXT"],
+    'tolerate_underscores_in_hostnames': True,
+    'feature_ipam_coupling': True,
+  },
+}
